0045934f211a74bbb3fc8c10d6652337ee7f65c4
Martin Jaskulla <martinjaskulla@icloud.com>
2022-04-02 23:23:23 +0200

    Add cors support and clean up package.json by fixing versions and moving "cors", "dotenv", "express" to devDependencies

--- app.ts
+++ app.ts
@@ -1,9 +1,12 @@
 import express, { Express } from 'express';
+import cors from 'cors';
 import { getRewardsRoutes } from './routes/rewards';
 import { Database } from './database/database';
 
 export function makeApp(database: Database) {
     const app: Express = express();
+
+    app.use(cors());
 
     app.use('/', getRewardsRoutes(database));
 
